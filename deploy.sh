5d930afa33dc8d0abb06908d34f0dd2d0b41d945
gosho-kazuya <gosho-kazuya@919.jp>
2017-06-25 16:01:25 +0900

    change deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-npm run production
+set -eux
+
+yarn run production
 
 # S3を使う場合
 # pip install awscli
@@ -11,7 +13,7 @@ npm run production
 
 cp -r dist /tmp
 git checkout gh-pages
-ls | grep -v CNAME | xargs rm -rf
+ls | grep -v CNAME | grep -v node_modules | xargs rm -rf
 cp -r /tmp/dist/* .
 git add .
 git commit 'release'
