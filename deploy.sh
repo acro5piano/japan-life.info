678d7dece4c3066344c58febf6eb8f9306d14997
gosho-kazuya <gosho-kazuya@919.jp>
2017-06-25 15:56:01 +0900

    add github page deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-pip install awscli
-aws configure set region ap-northeast-1
-
 npm run production
-aws s3 sync dist/ s3://www.yours3bucket.com/ --delete
+
+# S3を使う場合
+# pip install awscli
+# aws configure set region ap-northeast-1
+# aws s3 sync dist/ s3://www.yours3bucket.com/ --delete
+
+# Github Pages の場合
+
+cp -r dist /tmp
+git checkout gh-pages
+rm -rf *
+cp -r /tmp/dist/* .
+git add .
+git commit 'release'
+git push origin gh-pages
+git checkout master
