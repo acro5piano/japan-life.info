1a7bdc457141d305fedcbe2af7e98347225aa36b
gosho-kazuya <gosho-kazuya@919.jp>
2017-06-25 15:53:19 +0900

    dist!

--- src/app.js
+++
@@ -1,6 +0,0 @@
-require('./css/lib/normalize.css')
-require('./css/lib/skeleton.css')
-
-require('./css/index.css')
-
-require('./js/index.js')
--- src/css/variables.css.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  desktop: 'screen and (min-width: 769px)',
-  info: '#40cec0',
-}
